dd7ba870c7cbc451a7aa7ebde3dbb8ed4ba987e9
thallencar <alencar.thalita@gmail.com>
2024-11-04 17:51:43 -0300

    feat: atualizando camada data

---
+++ ImpactHub.Data/ImpactHubDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ImpactHub.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImpactHub.Data
+{
+    public class ImpactHubDbContext : DbContext
+    {
+        public ImpactHubDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<CadastroModel> Cadastros { get; set; }
+        public DbSet<ContatoModel> Contatos { get; set; }
+        public DbSet<EnderecoModel> Enderecos { get; set; }
+        public DbSet<ResultadoESGModel> Resultados { get; set; }
+        public DbSet<QuestionarioESGModel> Questionarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
